79189c26879a821b5b37576ac70538f108fb7e0e
Timur <timurmambetaliev2006@gmail.com>
2020-07-24 08:20:56 +0600

    Added Modal

--- Проект второго модуля/js/script.js
+++ Проект второго модуля/js/script.js
@@ -1,5 +1,6 @@
 window.addEventListener("DOMContentLoaded", () => {
-  // tabs
+  // Tabs
+
   let tabs = document.querySelectorAll(".tabheader__item"),
     tabsParent = document.querySelector(".tabheader__items"),
     tabsContent = document.querySelectorAll(".tabcontent");
@@ -92,5 +93,54 @@ window.addEventListener("DOMContentLoaded", () => {
   }
 
   setClock('.timer', deadLine);
+
+  // Modal
+
+  const modal = document.querySelector('.modal'),
+    modalBtns = document.querySelectorAll('[data-modalBtn]'),
+    modalCloseBtn = document.querySelector('[data-close]');
+
+  function openModal() {
+    modal.classList.add('show');
+    modal.classList.remove('hide');
+    document.body.style.overflow = 'hidden';
+    clearInterval(modalTimerId);
+  }
+
+  modalBtns.forEach(btn => {
+    btn.addEventListener('click', openModal);
+  });
+
+  function closeModal() {
+    modal.classList.add('hide');
+    modal.classList.remove('show');
+    document.body.style.overflow = '';
+  }
+
+
+  modalCloseBtn.addEventListener('click', closeModal);
+
+  modal.addEventListener('click', (e) => {
+    if (e.target === modal) {
+      closeModal();
+    }
+  });
+
+  document.addEventListener('keydown', (e) => {
+    if (e.keyCode == 27 && modal.classList.contains('show')) {
+      closeModal();
+    }
+  });
+
+  const modalTimerId = setTimeout(openModal, 5000);
+
+  function showModalByScroll() {
+    if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight) {
+      openModal();
+      window.removeEventListener('scroll', showModalByScroll);
+    }
+  }
+
+  window.addEventListener('scroll', showModalByScroll);
 
 });
